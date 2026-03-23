199193d55a745952c198c2bc6c47afeb1a041e5a
samyh ayoub <samyhayoub@MacBook-Air-van-samyh.local>
2026-03-23 19:20:46 +0100

    chore: update Header 2 component

--- components/Header 2.tsx
+++ components/Header 2.tsx
@@ -1,15 +1,21 @@
-import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
 import HeaderDesktopNav from "./HeaderDesktopNav";
+import { getStorefrontShellData } from "@/lib/storefront";
 
-const Header = () => {
+const Header = async () => {
+  const shell = await getStorefrontShellData();
+
   return (
     <header className="sticky top-0 z-50 border-b border-shop_light_green/15 bg-white/75 py-3.5 backdrop-blur-xl">
       <Container className="flex items-center justify-between gap-3 text-lightColor lg:grid lg:grid-cols-[auto_minmax(0,1fr)_auto] lg:items-center lg:gap-6">
         <div className="flex min-w-0 items-center gap-2.5">
-          <MobileMenu />
+          <MobileMenu
+            links={shell.headerLinks}
+            categories={shell.navigationCategories}
+            socialLinks={shell.socialLinks}
+          />
           <Logo />
         </div>
         <HeaderDesktopNav />
@@ -19,4 +25,3 @@ const Header = () => {
 };
 
 export default Header;
-
